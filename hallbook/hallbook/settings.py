8f1209c75d3a40628aec88f8bd820d0c6a3e1880
nischalmainali21 <nischalmainali21@gmail.com>
2023-02-28 17:57:23 +0545

    Added corsMiddlewaere and allowed origins     Needed to connect with frontend

--- hallbook/hallbook/settings.py
+++ hallbook/hallbook/settings.py
@@ -46,11 +46,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'hallbook.urls'
