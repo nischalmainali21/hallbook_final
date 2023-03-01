ec3572f3cb04851870502de79bf142af9833655f
nischalmainali21 <nischalmainali21@gmail.com>
2023-03-01 19:50:10 +0545

    Added function to updateToken     Has not been used

--- hallbookreact/src/context/AuthContext.js
+++ hallbookreact/src/context/AuthContext.js
@@ -1,60 +1,105 @@
-import {useState,useEffect, createContext} from 'react'
+import { useState, useEffect, createContext } from "react";
 import jwt_decode from "jwt-decode";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export default AuthContext;
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
+  let [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
+      : null
+  );
+  let [user, setUser] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? jwt_decode(localStorage.getItem("authTokens"))
+      : null
+  );
 
-    
-    let [authTokens,setAuthTokens] = useState(() => localStorage.getItem('authTokens')?JSON.parse(localStorage.getItem('authTokens')):null)
-    let [user,setUser] = useState(() => localStorage.getItem('authTokens')?jwt_decode(localStorage.getItem('authTokens')):null)
+  //   let [loading, setLoading] = useState(true);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    let loginUser = async (e) => {
-        
-        let response = await fetch("http://127.0.0.1:8000/api/user/login/",{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({'user_type':'student','email':e.target.userName.value,'password':e.target.password.value}) //user_type
-        })
-        
+  let loginUser = async (e) => {
+    let response = await fetch("http://127.0.0.1:8000/api/user/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_type: "student",
+        email: e.target.userName.value,
+        password: e.target.password.value,
+      }), //user_type
+    });
 
-        let data = await response.json()
-        // console.log("data",data)
-        // console.log("🚀 ~ file: AuthContext.js:21 ~ loginUser ~ response:", response)
-        if(response.status === 200){
-            setAuthTokens(data)
-            setUser(jwt_decode(data.access))
-            localStorage.setItem('authTokens',JSON.stringify(data))
-            navigate('/about')
-        }else{
-            alert('something is wrong')
+    let data = await response.json();
+    // console.log("data",data)
+    // console.log("🚀 ~ file: AuthContext.js:21 ~ loginUser ~ response:", response)
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      navigate("/about");
+    } else {
+      alert("something is wrong");
+    }
+  };
+
+  let logoutUser = () => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem("authTokens");
+    navigate("/login");
+  };
+
+  /*let updateToken = async () => {
+    console.log("update token called");
+    let response = await fetch("http://127.0.0.1:8000/api/user/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refresh: authTokens?.refresh }),
+    });
+    let data = await response.json();
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+    } else {
+      logoutUser();
+    }
+        if(loading){
+            setLoading(false)
         }
-        
-    }
+  };*/
 
-    let logoutUser = () => {
-        setAuthTokens(null)
-        setUser(null)
-        localStorage.removeItem('authTokens')
-        navigate('/login')
-    }
-    
-    let contextData = {
-        user:user,
-        loginUser:loginUser,
-        logoutUser:logoutUser,
-    }
+  let contextData = {
+    user: user,
+    authTokens: authTokens,
+    loginUser: loginUser,
+    logoutUser: logoutUser,
+  };
 
-    return (
-        <AuthContext.Provider value={contextData}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  /*   useEffect(() => {
+
+         if(loading){
+             updateToken()
+         }
+
+     let fourMinutes = 4*60*1000
+    let interval = setInterval(() => {
+       if (authTokens) {
+      updateToken();
+     }
+    },fourMinutes);
+    return ()=> clearInterval(interval)
+   }, [authTokens, loading]); */
+
+  return (
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  );
+};
