67b067f651d9db76bbeb20b07ff70a6ad6cb0621
nischalmainali21 <nischalmainali21@gmail.com>
2023-03-03 17:16:12 +0545

    AdminPage setup completed.

---
+++ hallbookreact/src/components/Admin/AdminHallCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import CButton from "../CButton";
+
+function AdminHallCard({ id, name, capacity, location }) {
+  return (
+    <div>
+      <div className="flex flex-col rounded-lg p-4 shadow-lg md:flex-row md:items-center">
+        <div className="flex h-32 w-full flex-col gap-4 p-2 md:h-28">
+          <div>
+            <div className="text-3xl font-bold">{name}</div>
+            <div className="text-sm text-gray-500">Capacity: {capacity}</div>
+            <div className="text-sm text-gray-500">Location: {location}</div>
+          </div>
+        </div>
+        <div className="flex items-center gap-10">
+          <div>
+            <CButton id={`editHall${id}`} type="button" btnDesc="Edit Hall" />
+          </div>
+          <div>
+            <button
+              id={`deleteHall${id}`}
+              type="button"
+              className="shadow-mdtransition mx-auto block rounded-lg bg-red-500 px-6 py-4 text-base 
+              font-medium uppercase leading-tight text-white duration-150 ease-in-out hover:bg-red-700 hover:shadow-lg
+               focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg md:py-3"
+            >
+              Delete Hall
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminHallCard;
--- hallbookreact/src/pages/AdminPage.js
+++ hallbookreact/src/pages/AdminPage.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import AdminHallCard from "../components/Admin/AdminHallCard";
+
+const buttonfixedclass = `buttonfixedclass`
 
 function AdminPage() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let getHallList = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/hall/halls/");
+        if (response.ok) {
+          const data = await response.json();
+          setData(data);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("error", error);
+        setError(error);
+      }
+    };
+    getHallList();
+  }, []);
+
+  if (loading) return "Loading";
+  if (error) return "error";
+
   return (
-    <div>AdminPage goes here</div>
-  )
+    <div className="md:grid grid-cols-[200px_1fr]">
+      <div className="mt-6 md:mt-16">
+        <div>
+          <button className={buttonfixedclass+` bg-green-500 text-black hover:bg-green-700 focus:bg-green-700 active:bg-green-800`}>
+                Add New Hall
+          </button>
+        </div>
+      </div>
+      <div className="mx-auto  mt-2 md:mt-14 flex min-h-screen max-w-3xl flex-col gap-12 p-2  md:w-2/3 md:gap-8">
+        {data.map((item) => (
+          <AdminHallCard
+            key={item.id}
+            id={item.id}
+            name={item.hallName}
+            capacity={item.capacity}
+            location={item.location}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
