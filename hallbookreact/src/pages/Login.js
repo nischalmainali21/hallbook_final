fbb911f6e075cad5e238bf63dea53203f2492847
nischalmainali21 <nischalmainali21@gmail.com>
2023-03-03 09:57:04 +0545

    Updated Login page

--- hallbookreact/src/pages/Login.js
+++ hallbookreact/src/pages/Login.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoginForm from "../components/Login/LoginForm";
 import { useState } from "react";
 import useAuth from "../hooks/useAuth";
