b265c54fd039d5cbb6b9ff6a975cf65c371f3cdb
nischalmainali21 <nischalmainali21@gmail.com>
2023-03-04 12:15:43 +0545

    facultypage setup

--- hallbookreact/src/pages/About.js
+++ hallbookreact/src/pages/About.js
@@ -1,13 +1,10 @@
-import { useContext } from "react"
-import AuthContext from "../context/AuthContext"
-export default function About(){
-
-    let {user} = useContext(AuthContext)
-    return (
-        <div>
-            About goes here
-            {user && <p>Hello {user.user_id}</p>}
-            
-        </div>
-    )
+import useAuth from "../hooks/useAuth";
+export default function About() {
+  let { user } = useAuth();
+  return (
+    <div className="mx-auto mt-10 flex min-h-screen max-w-4xl flex-col gap-12 p-2 md:w-2/3 md:gap-8 rounded-lg shadow-lg">
+      About goes here
+      {user && <p>Hello {user.user_id}</p>}
+    </div>
+  );
 }
