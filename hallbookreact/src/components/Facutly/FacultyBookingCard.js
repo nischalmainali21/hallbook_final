bdd3d0ce91678cf8ddd4229c7ab5287e30d268f5
nischalmainali21 <nischalmainali21@gmail.com>
2023-03-06 13:02:51 +0545

    Now shows the name of bookedHall in FacultyBookingCard

--- hallbookreact/src/components/Facutly/FacultyBookingCard.js
+++ hallbookreact/src/components/Facutly/FacultyBookingCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import CButton from "../CButton";
+import useFetch from "../../hooks/useFetch";
+import { useState } from "react";
 
 function FacultyBookingCard({
   id,
@@ -9,18 +11,35 @@ function FacultyBookingCard({
   endTime,
   verified,
   handleVerifyClick,
-  handleRejectClick
+  handleRejectClick,
 }) {
   const buttonfixedclass = `buttonfixedclass`;
+  const [bookedHallName, setBookedHallName] = useState('')
+
+  let api = useFetch();
+  let getHallData = async () => {
+    try {
+      let { response, data } = await api(`/api/hall/halls/${bookedHallID}/`, {
+        method: "GET",
+      });
+      if (response.ok) {
+        setBookedHallName(data.hallName)
+      }
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  getHallData()
+  console.log(bookedHallName)
 
   return (
     <div>
       <div className="flex flex-col justify-between rounded-lg p-2 shadow-lg md:flex-row md:items-center md:justify-between">
-        
         <div className="flex h-36  flex-col gap-4 p-1 md:h-32">
           <div>
             <div className="text-3xl font-bold">Event Name:{eventID}</div>
-            <div className="text-md">BookedHall: {bookedHallID}</div>
+            <div className="text-md">BookedHall: {bookedHallName}</div>
             <div className="text-md">
               EventTime: {`${startTime}-${endTime}`}
             </div>
@@ -32,22 +51,29 @@ function FacultyBookingCard({
             {verified ? "verified" : "unverified"}
           </div>
         </div>
-        {verified?"":
-        <div className="flex md:gap-10 ">
-          <CButton id={`verifyBooking${id}`} type="button" btnDesc="Verify" onClick={()=>handleVerifyClick(id)}/>
-          <button
-            id={`rejectBooking${id}`}
-            type="button"
-            onClick={()=>handleRejectClick(id)}
-            className={
-              buttonfixedclass +
-              ` bg-red-500 text-white hover:bg-red-700 focus:bg-red-700 active:bg-red-800`
-            }
-          >
-            Reject
-          </button>
-        </div>
-}
+        {verified ? (
+          ""
+        ) : (
+          <div className="flex md:gap-10 ">
+            <CButton
+              id={`verifyBooking${id}`}
+              type="button"
+              btnDesc="Verify"
+              onClick={() => handleVerifyClick(id)}
+            />
+            <button
+              id={`rejectBooking${id}`}
+              type="button"
+              onClick={() => handleRejectClick(id)}
+              className={
+                buttonfixedclass +
+                ` bg-red-500 text-white hover:bg-red-700 focus:bg-red-700 active:bg-red-800`
+              }
+            >
+              Reject
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
