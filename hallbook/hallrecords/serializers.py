975aa9b09ba3c6591d285f3aba124789a53b08b5
Prakash-coder <chaulagainprakash78@gmail.com>
2023-03-07 23:58:58 +0545

    email field added and put request modified

--- hallbook/hallrecords/serializers.py
+++ hallbook/hallrecords/serializers.py
@@ -28,14 +28,49 @@ class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ('id', 'eventManager', 'eventName', 'eventDate', 'startTime', 'endTime', 'bookedHall', 'organizingClub', 'EventDetailFile', 'EventDetailText', 'PhoneNumber')
+        fields = ('id', 'eventManager', 'eventName', 'eventDate', 'startTime', 'endTime', 'bookedHall', 'organizingClub', 'EventDetailFile', 'EventDetailText', 'PhoneNumber','email')
         
+class EventSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Event model.
+    """
+    bookedHall = serializers.PrimaryKeyRelatedField(queryset=Hall.objects.all())
+
+    class Meta:
+        model = Event
+        fields = ('id', 'eventManager', 'eventName', 'eventDate', 'startTime', 'endTime', 'bookedHall', 'organizingClub', 'EventDetailFile', 'EventDetailText', 'PhoneNumber','email')
+
 
     def validate(self, data):
         """
-        Check if the selected hall is available for the requested time period.
+        Check that the start time and end time of the event do not clash with other events
         """
-        bookedHall = data['bookedHall']
-        if not bookedHall.is_available(data['startTime'], data['endTime'],data['eventDate']):
-            raise serializers.ValidationError('Hall is already booked for this time period.')
+        # Get the event data from the request data
+        event_id = self.context.get('view').kwargs.get('pk')
+        event_start_time = data['startTime']
+        event_end_time = data['endTime']
+        event_date = data['eventDate']
+        booked_hall = data['bookedHall']
+
+        # If the request method is PUT, exclude the current event from the queryset
+        events = Event.objects.all()
+
+        if event_id:
+            events = events.exclude(id=event_id)
+
+        # Check if any other event is booked in the same hall for the given time and date
+        conflicting_events = events.filter(
+            bookedHall=booked_hall,
+            eventDate=event_date,
+            startTime__lt=event_end_time,
+            endTime__gt=event_start_time
+        )
+
+        if conflicting_events.exists():
+            if event_id:
+                conflicting_events = conflicting_events.exclude(id=event_id)
+
+            if conflicting_events.exists():
+                raise serializers.ValidationError('This hall is not available for the given time period')
+
         return data
